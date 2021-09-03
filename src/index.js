160d55b8bf7de1a5b088a127e2fcf1191ebeaa70
TroitskiyAndrew <TroitskiyA@yandex.ru>
2021-09-03 22:49:24 +0500

    complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array == undefined || array.length ==0)
+    return 0;
+  for (let num of array) {
+    if (array.find((x) => { return x < num }) !== undefined)
+      continue;
+    return num
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length ==0)
+    return 0;
+  for (let num of array) {
+    if (array.find((x) => { return x > num }) !== undefined)
+      continue;
+    return num
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length ==0)
+    return 0;
+  return array.reduce((a,b) => a+b,0)/array.length
 }
